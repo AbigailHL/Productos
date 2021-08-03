2de769182767c2c3e671af66fe630bb65617c04e
Abigail Hernandez <abigailhdezleon@gmail.com>
2021-08-03 00:02:02 -0500

    tercer commit, limpieza de codigo

--- src/main/java/com/productos/configuration/SwaggerConfig.java
+++ src/main/java/com/productos/configuration/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.productos.configuration;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +13,6 @@ import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-import java.util.ArrayList;
 
 
 /**
@@ -37,7 +37,8 @@ public class SwaggerConfig {
 
     private ApiInfo metaInfo() {
 
-        ApiInfo apiInfo = new ApiInfo(
+        @SuppressWarnings("rawtypes")
+		ApiInfo apiInfo = new ApiInfo(
                 "API REST Productos",
                 "API REST de Abigail Hernandez",
                 "1.0",
